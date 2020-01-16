068aef10015bff5916c1317ed89185565e31de47
Tripple-A <biodun9@gmail.com>
2020-01-16 16:23:29 +0100

    added linting

--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,6 @@ const menu = () => {
    const menu = document.createElement('div')
    menu.innerHTML = 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloremque commodi eos voluptatem numquam pariatur deleniti repellat fugiat eligendi nulla molestiae sunt praesentium vero sequi distinctio error quibusdam maiores natus magnam, explicabo hic sed alias dolores, quis eum! Reprehenderit atque cupiditate dolorum? Saepe, doloribus veniam? Nulla!'
     menu.classList.add('menu')
-    content.appendChild(menu)
     return menu
 }
 
