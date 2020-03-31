f3364e68c87267010545d566f299a8fe3441a0a1
Tripple-A <biodun9@gmail.com>
2020-03-31 12:50:12 +0100

    added readme image

---
+++ src/temp.js
@@ -0,0 +1,14 @@
+let newValue;
+
+const celcius = (num) => {
+  newValue = Math.round((parseFloat(num, 10) - 32) * (5 / 9));
+  return newValue;
+};
+
+const farenheit = (num) => {
+  newValue = Math.round((parseFloat(num, 10) * 9) / 5 + 32);
+  return newValue;
+};
+
+export default farenheit;
+//export { celcius, farenheit };
